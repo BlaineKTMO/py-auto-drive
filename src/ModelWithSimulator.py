f5868be8edfd9461c3f3a59d6059efae3e563fa1
BlaineKTMO <Blaine.Oania@gmail.com>
2023-04-17 09:06:00 -0400

    Began testing, seems to not converge

--- src/ModelWithSimulator.py
+++ src/ModelWithSimulator.py
@@ -1,7 +1,7 @@
 import torch
-import math
 import PyBotSim.src.simulator as Sim
 import src.PolicyGradAgent as Agent
+
 
 class Model:
     def __init__(self, inputSize, outputSize, hiddenSize, learningRate):
@@ -13,36 +13,30 @@ class Model:
         distance = ((robot_pos[0] - goal_pos[0]) ** 2 + (robot_pos[1] - goal_pos[1]) ** 2) ** 0.5
         return -distance
 
-    def run_episode(self):
-        self.sim.reset()
-        for _ in range(50):
-            input, theta, dist, vl, vr, running = self.sim.step()
-        input.append(theta)
-        input.append(vl)
-        input.append(vr)
+    def run_episode(self, maxSteps=1000, draw=False):
         rewards = []
         log_probs = []
+        self.sim.reset()
+        for _ in range(50):
+            input, dist, running, collision = self.sim.step(draw=draw)
 
-        count = 0
-        while running and count < 1000:
+        step = 0
+        while running and step < maxSteps:
             action, action_prob = self.agent.select_action(input)
             # print(action_prob)
-            input, theta, dist, vl, vr, running = self.sim.step(action)
+            input, dist, running, collision = self.sim.step(action=action,draw=draw)
 
-            input.append(theta)
-            input.append(vl)
-            input.append(vr)
             # print(input)
 
             # Compute reward based on progress towards goal
             reward = -abs(dist)
-            if (not running):
-                reward = -1
+            if (collision):
+                reward = -2
             rewards.append(reward)
             log_probs.append(action_prob)
-            # print(f"Reward: {reward}")
+            print(f"Reward: {reward}")
             # print(f"theta: {theta}")
-            count += 1
+            step += 1
 
         # Update policy using accumulated rewards and log probabilities
         self.agent.update_policy(rewards, log_probs)
@@ -53,7 +47,27 @@ def main():
     hiddenSize = 100
     outputSize = 5
     learningRate = 0.01
+    maxSteps = 2000
 
     model = Model(inputSize, outputSize, hiddenSize, learningRate)
+
+    checkpoint = torch.load("model.ckpt")
+    # model.agent.policy_network.load_state_dict(checkpoint['net_stat_dict'])
+    # model.agent.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    # epoch = checkpoint['epoch']
+    
+    model.agent.policy_network.load_state_dict(checkpoint)
+
+    # Training Loop
+    epoch = 0
     while True:
-        model.run_episode()
+        if epoch % 10 == 0:
+            model.run_episode(maxSteps=maxSteps, draw=True)
+            torch.save({
+                           'net_state_dict': model.agent.policy_network.state_dict(),
+                            'optimizer_state_dict': model.agent.optimizer.state_dict(),
+                            'epoch': epoch}, "model2.ckpt")
+            pass
+        else:
+            model.run_episode()
+        epoch += 1
--- src/PolicyGradAgent.py
+++ src/PolicyGradAgent.py
@@ -14,25 +14,24 @@ class PolicyGradAgent:
 
     def select_action(self, input):
         # Picking from policy
-        # with torch.no_grad():
-        #     action = self.policy_network(torch.FloatTensor(input))
-        #     action_dist = torch.distributions.Categorical(action_probs)
-        #     action = action_dist.sample()
-        # return action.item(), action_probs[action]
         with torch.no_grad():
-            action_mean_l, action_mean_r, d1, d2, d3 = self.policy_network(torch.FloatTensor(input))
-            action_mean = torch.tensor([action_mean_l, action_mean_r], requires_grad=True)
             softplus = torch.nn.Softplus()
-            d1 = softplus(d1)
-            d2 = softplus(d2)
-            d3 = softplus(d3)
-            d = torch.tensor([d1, d2, d3])
+
+            action_mean_l, action_mean_r, d1, d2, d3 = self.policy_network(torch.FloatTensor(input))
+
+            action_mean = torch.tensor([action_mean_l, action_mean_r], requires_grad=True)
+            d = softplus(torch.tensor([d1, d2, d3], requires_grad=True))
+
+            # Calculate covariance matrix
             cov = torch.zeros(2, 2)
             cov[torch.tril_indices(2, 2, offset=0).tolist()] = d
             cov = cov * torch.transpose(cov, 0, 1)
+
+            # Create MVN and sample action
             action_dist = torch.distributions.MultivariateNormal(action_mean, cov + eps)
             action = action_dist.sample()
             # print(action_dist.log_prob(action))
+
         return action.numpy(), action_dist.log_prob(action).unsqueeze(0)
 
     def update_policy(self, rewards, log_probs):
@@ -44,16 +43,15 @@ class PolicyGradAgent:
             # Calculate discounted rewards
             cumulative_reward = reward + discount_factor * cumulative_reward
             discounted_rewards.insert(0, cumulative_reward)
-
         discounted_rewards = torch.FloatTensor(discounted_rewards)
+
         # Normalize rewards
         discounted_rewards = (discounted_rewards - discounted_rewards.mean()) / (discounted_rewards.std() + 1e-9)
 
+        # Calculate policy loss
         policy_loss = []
-        print(log_probs)
         for log_prob, reward in zip(log_probs, discounted_rewards):
-            policy_loss.append(-log_prob * reward)
-        print(policy_loss)
+            policy_loss.append(-abs(-log_prob * reward))
         policy_loss = torch.cat(policy_loss).sum()
 
         # Optimization
